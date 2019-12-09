1e061a85e1c626d1ec761dbf9190bdc85465b552
Julius Ortiz <jay.ortiz@dice205.com>
2019-12-09 13:13:04 +0800

    Initial commit

---
+++ src/test/java/GenericTest/LoginSales.java
@@ -0,0 +1,49 @@
+package GenericTest;
+import org.openqa.selenium.By;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import Configuration.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class LoginSales extends SetUp {
+
+    @Test(priority = 0)
+    public void Login() throws InterruptedException, IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("WebElements.xlsx");
+        assert inputStream != null;
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        Select businesstype = new Select(driver.findElement(By.xpath(String.valueOf(sheet.getRow(0).getCell(0)))));
+        businesstype.selectByVisibleText(String.valueOf(sheet.getRow(0).getCell(1)));
+
+        driver.findElement(By.xpath(String.valueOf(sheet.getRow(1).getCell(0))))
+                .sendKeys(String.valueOf(sheet.getRow(5).getCell(1)));
+        driver.findElement(By.xpath(String.valueOf(sheet.getRow(6).getCell(0))))
+                .sendKeys(String.valueOf(sheet.getRow(6).getCell(1)));
+        driver.findElement(By.xpath(String.valueOf(sheet.getRow(7).getCell(0)))).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.out.println(CYAN_BOLD_BRIGHT + "Login Sales = PASSED" + RESET);
+        System.out.println();
+        driver.quit();
+       /* WebElement Header = driver.findElement(By.xpath(String.valueOf(sheet.getRow(2).getCell(4))));
+        Assert.assertTrue(Header.isDisplayed());
+        System.out.println("Login Customer = PASSED");
+        driver.quit();*/
+    }
+}
+
+
+
+
+
+
+
+
+
