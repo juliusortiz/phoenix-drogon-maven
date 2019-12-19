1d14f7e5639d4116e1709e93b9b10cb7f96dd552
Lester <dice205test@gmail.com>
2019-12-19 11:51:42 +0800

    Added test navbar
    Navigation

--- src/test/java/GenericTest/LoginAdmin.java
+++ src/test/java/GenericTest/LoginAdmin.java
@@ -6,11 +6,11 @@ import java.util.concurrent.TimeUnit;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import Configuration.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 
 
 public class LoginAdmin extends SetUp {
@@ -32,12 +32,16 @@ public class LoginAdmin extends SetUp {
         driver.findElement(By.xpath(String.valueOf(sheet.getRow(7).getCell(0)))).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        driver.findElement(By.xpath("/html/body/div[2]/main/header/button[1]/span")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/aside/div/nav/a[1]/div/div/label")).click();
+        ///html/body/div[2]/aside/div/nav/a[1]/div/div/label
+
         WebElement dashboardchart = driver.findElement(By.cssSelector(String.valueOf(sheet.getRow(1).getCell(5))));
         Assert.assertTrue(dashboardchart.isDisplayed());
         System.out.print(CYAN_BOLD_BRIGHT + "Login Admin = PASSED" + RESET);
         System.out.println();
 
-        driver.quit();
+        //driver.quit();
     }
 }
 
