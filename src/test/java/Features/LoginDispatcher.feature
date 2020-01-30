dc9bdbf95b846e91f9a5ac1e8f108aafc2f3d652
Lester <dice205test@gmail.com>
2020-01-30 08:10:28 +0800

    update
    dispatcher_user

---
+++ src/test/java/Features/LoginDispatcher.feature
@@ -0,0 +1,11 @@
+@SmokeTest2
+Feature: Login Dispatcher
+  This feature deals with the admin page accessibility and navigations
+
+  Scenario: Login dispatcher user and navigate to all pages
+    Given logged in as dispatcher user
+    And navigate to Problems Reported page
+    And navigate to Dispatcher Settings page
+    And navigate to Dispatcher Notification page
+
+    Then Dispatcher Logout
---
+++ src/test/java/StepClasses/LoginDispatcher1.java
@@ -0,0 +1,99 @@
+package StepClasses;
+
+import Configuration.SetUp;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+public class LoginDispatcher1 extends SetUp {
+    @Given("^logged in as dispatcher user$")
+    public void loggedInAsDispatcherUser() throws IOException, InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+//      options.addArguments("--headless");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-gpu");
+        driver = new ChromeDriver(options);
+
+        driver.get("https://phoenix_epod_app.dice205.asia/");
+        driver.getWindowHandle();
+        driver.manage().window().maximize();
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("Credentials.xlsx");
+        assert inputStream != null;
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        Select businesstype = new Select(driver.findElement(By.xpath(String.valueOf(sheet.getRow(0).getCell(0)))));
+        businesstype.selectByVisibleText(String.valueOf(sheet.getRow(0).getCell(1)));
+
+        driver.findElement(By.xpath("//input[@name='username']"))
+                .sendKeys(String.valueOf(sheet.getRow(3).getCell(1)));
+        driver.findElement(By.xpath("//input[@name='pass']"))
+                .sendKeys(String.valueOf(sheet.getRow(8).getCell(1)));
+        driver.findElement(By.xpath("//button[@class='login100-form-btn']")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.out.println(CYAN_BOLD_BRIGHT + "Login Dispatcher = PASSED" + RESET);
+        System.out.println();
+       /* WebElement Header = driver.findElement(By.xpath(String.valueOf(sheet.getRow(2).getCell(4))));
+        Assert.assertTrue(Header.isDisplayed());
+        System.out.println("Login Customer = PASSED");
+        driver.quit();*/
+        Thread.sleep(4000);
+    }
+
+    @And("^navigate to Problems Reported page$")
+    public void navigateToProblemsReportedPage() throws InterruptedException {
+        driver.findElement(By.xpath("/html/body/div[2]/main/header/button[1]/span")).click(); //dashboard
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body/div[2]/aside/div/nav/a[2]/div/div/span")).click(); //problems reported page
+        WebElement problemsreportedpage = driver.findElement(By.xpath("/html/body/div[2]/aside/div/nav/a[2]/div/div/span")); //
+        Assert.assertTrue(problemsreportedpage.isDisplayed());
+        System.out.print(CYAN_BOLD_BRIGHT + "Navigation to Problems Reported Page = PASSED" + RESET);
+        System.out.println();
+        Thread.sleep(2000);
+    }
+
+    @And("^navigate to Dispatcher Settings page$")
+    public void navigateToDispatcherSettingsPage() throws InterruptedException {
+        driver.findElement(By.xpath("/html/body/div[2]/main/header/button[1]/span")).click(); //dashboard
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body/div[2]/aside/div/a/span")).click(); //settings page
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body/div[2]/main/section/div/section/article/header/a[2]")).click(); //send feedback
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body/div[2]/main/section/div/section/article/header/a[3]")).click(); //about this app
+        WebElement settingspage = driver.findElement(By.xpath("/html/body/div[2]/main/section/div/section/article/header/a[3]")); //
+        Assert.assertTrue(settingspage.isDisplayed());
+        System.out.print(CYAN_BOLD_BRIGHT + "Navigation to Settings Page = PASSED" + RESET);
+        System.out.println();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @And("^navigate to Dispatcher Notification page$")
+    public void navigateToDispatcherNotificationPage() throws InterruptedException {
+            driver.findElement(By.xpath("/html/body/div[2]/main/header/a[2]")).click(); //notfication page
+            WebElement notificationpage = driver.findElement(By.xpath("/html/body/div[2]/main/header/a[2]")); //
+            Assert.assertTrue(notificationpage.isDisplayed());
+            System.out.print(CYAN_BOLD_BRIGHT + "Navigation to Notification Page = PASSED" + RESET);
+            System.out.println();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Then("^Dispatcher Logout$")
+    public void dispatcherLogout() throws InterruptedException {
+
+    }
+}
